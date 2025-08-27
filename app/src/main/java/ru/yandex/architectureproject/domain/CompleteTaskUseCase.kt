7b8523c9ba85c8be99424b972cbbf90da0653ed3
Крайнюков Сергей <sergey.kraynyukov@gazprombank.ru>
2025-08-27 22:18:02 +0300

    refactor

--- app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
+++ app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
@@ -1,12 +1,15 @@
 package ru.yandex.architectureproject.domain
 
+import kotlinx.coroutines.delay
 import ru.yandex.architectureproject.data.repository.TaskRepository
 
+private const val DELETE_TASK_DELAY_MILLIS = 2_000L
 class CompleteTaskUseCase(
     private val repository: TaskRepository,
 ) {
     suspend operator fun invoke(taskId: Int) {
         repository.completeTask(taskId)
-        // TODO: Здесь будет автоудаление задачи
+        delay(DELETE_TASK_DELAY_MILLIS)
+        repository.deleteTask(taskId)
     }
 }
